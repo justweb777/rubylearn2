cfc74437c42aec24e9f8e9e96b437057fb63c9bd
justweb777 <justweb777@gmail.com>
2011-03-10 19:42:41 +0800

    new rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubylearn2::Application.config.secret_token = '07592c73864e95824efcd39ccb25b24c67623cb484fbb39f66107ad25802b2d0a319f363b95b687fb57a041a685c0335d4d39c66757babdb18436737193fbb51'
